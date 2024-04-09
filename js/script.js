72fb62651e46ac0cfad1f701aba4c9829d8f34e7
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-04-09 18:18:37 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function calculates 
+ */
+function calculate() {
+  // input
+  const radius = parseFloat(document.getElementById('radius').value)
+
+  // process
+  const volume = 4 / 3 * Math.PI * Math.pow(radius, 3)
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+}
